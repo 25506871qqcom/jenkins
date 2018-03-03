25e362ba2aad921cbea91c7146a7e2fa6cc4579b
lyp <25506871@qq.com>
2018-03-03 14:56:24 +0800

    for war in tomcat

--- src/main/java/controller/SampleController.java
+++ src/main/java/controller/SampleController.java
@@ -2,6 +2,7 @@ package controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
-public class SampleController {
+public class SampleController extends SpringBootServletInitializer {
     @RequestMapping("/")
     @ResponseBody
     String home() {
         return "Hello World!";
     }
+
+
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SampleController.class, args);
