d58808dfa0771075882781a141fb87e7594aba86
lyp <25506871@qq.com>
2018-03-03 14:57:38 +0800

    add source

--- src/main/java/controller/SampleController.java
+++ src/main/java/controller/SampleController.java
@@ -2,6 +2,7 @@ package controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,10 @@ public class SampleController extends SpringBootServletInitializer {
         return "Hello World!";
     }
 
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SampleController.class);
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SampleController.class, args);
