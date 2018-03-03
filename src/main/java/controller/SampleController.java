b61f6835cdf5bfb4ea37e818deaa97b763c4f860
lyp <25506871@qq.com>
2018-03-03 15:52:52 +0800

    234

--- src/main/java/controller/SampleController.java
+++ src/main/java/controller/SampleController.java
@@ -18,7 +18,7 @@ public class SampleController extends SpringBootServletInitializer {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello 23 World!";
+        return "Hello 234 World!";
     }
 
     @Override
